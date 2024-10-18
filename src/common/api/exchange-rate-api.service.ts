f9021e74191eac4e875233dacb4bec87082555f0
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-10-18 15:28:07 +0200

    feat: Add exchange rate api service

---
+++ src/common/api/exchange-rate-api.service.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+import { Currency, CurrencyGetExchangeRatesResponse, ExchangeRateApi } from './exchange-rate-api.types';
+
+class ExchangeRateApiService implements ExchangeRateApi {
+  private static instance: ExchangeRateApiService;
+  private readonly exchangeRateApiBaseUrl = 'https://v6.exchangerate-api.com/v6';
+
+  // Private constructor to prevent direct instantiation
+  private constructor() {}
+
+  public static getInstance(): ExchangeRateApiService {
+    if (!ExchangeRateApiService.instance) {
+      ExchangeRateApiService.instance = new ExchangeRateApiService();
+    }
+
+    return ExchangeRateApiService.instance;
+  }
+
+  public getExchangeRateForCurrency(baseCurrency: Currency): Promise<CurrencyGetExchangeRatesResponse> {
+    return axios
+      .get<CurrencyGetExchangeRatesResponse>(`${this.exchangeRateApiBaseUrl}/latest/${baseCurrency}`, { headers: this.authHeader() })
+      .then((response) => response.data)
+      .catch((error) => error.data);
+  }
+
+  private authHeader(): Record<string, string> {
+    return {
+      Authorization: `Bearer ${this.apiToken()}`,
+    };
+  }
+
+  private apiToken(): string {
+    const token = process.env.REACT_APP_EXCHANGE_API_KEY;
+
+    if (!token) {
+      // TODO: Consider handling this more gracefully
+      throw new Error('Bearer token not found in environment variables.');
+    }
+
+    return token;
+  }
+}
+
+export default ExchangeRateApiService;
