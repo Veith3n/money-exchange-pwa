1d9496b373c8b430f8b557eb3cd0f7eaa9d495a3
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-10-26 00:38:11 +0200

    feat: Define notifications classes

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -9,11 +9,36 @@
 // service worker, and the Workbox build step will be skipped.
 
 import ExchangeRateApiService from '@common/api/exchange-rate-api.service';
+import { Currency } from '@common/api/exchange-rate-api.types';
 import { clientsClaim, setCacheNameDetails } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
+
+export class ConversionNotification {
+  static readonly type = 'SHOW_CONVERSION_NOTIFICATION';
+
+  constructor(readonly data: { baseCurrency: string; targetCurrency: string; baseCurrencyAmount: number; targetCurrencyAmount: number }) {
+    this.data = data;
+  }
+
+  get type() {
+    return ConversionNotification.type;
+  }
+}
+
+export class ExchangeRateNotification {
+  static readonly type = 'SHOW_EXCHANGE_RATE_NOTIFICATION';
+
+  constructor(readonly data: { baseCurrency: Currency; targetCurrency: Currency }) {
+    this.data = data;
+  }
+
+  get type() {
+    return ExchangeRateNotification.type;
+  }
+}
 
 declare const self: ServiceWorkerGlobalScope;
 
